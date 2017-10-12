195f1b2d5b433a04e3e9950ab8408e7e80c1eb26
Sunguk Ham <ham2959@gmail.com>
2017-10-11 23:48:04 -0700

    C lab

--- C/lab05.c
+++ C/lab05.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #define LINESIZE 1024
 
 int get_word(const char prompt[], char word[], size_t n);
@@ -9,13 +10,29 @@ int get_int(const char prompt[], int *p);
 
 int main(void) {
 
-    char prompt[] = "Enter: ";
-    size_t n = 9;
-    char word[9];
+    char prompt1[] = "Enter the student ID: ";
+    char prompt2[] = "Enter the grades of the student: ";
+    size_t n = 10;
+    char studentId[10];
+    int grade = 0;
+    int *gradeP = &grade;
 
     do {
-        get_word(prompt, word, n);
-    } while (is_valid_id(word) == 0);
+        get_word(prompt1, studentId, n);
+    } while (!is_valid_id(studentId));
+    
+    get_int(prompt2, gradeP);
+    
+    FILE *file = fopen("lab05.txt", "a");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+    
+    fprintf(file, "%s %3d ", studentId, grade);
+    
+    fclose(file);
+
 
 return 0;
 }
@@ -33,10 +50,9 @@ int get_word(const char prompt[], char word[], size_t n) {
             clearerr(stdin);
             return 0;
         }
-        if((sscanf(line, "%s", temp)) == 1 && (strlen(temp) < n)) {
-                strcpy(word, temp);
-                /*printf("%s\n", word);*/
-                return 1;
+        if((sscanf(line, "%s", temp) == 1) && (strlen(temp) < n)) {
+            strcpy(word, temp);
+            return 1;
         }
     }
 
@@ -49,26 +65,23 @@ int get_word(const char prompt[], char word[], size_t n) {
 check the first char
 check the 1-8*/
 int is_valid_id(const char s[]) {
-    size_t maxLength = 9;
-    int boolean = 0;
+    
     int i = 0;
-    int length = strlen(s);
 
-    if (strlen(s) == maxLength) {
-        if (s[0] == 'A' || s[0] == 'a') {
-            for (i = 1; i < length; i++) {
-                if (isdigit(s[i])) {
-                    boolean = 1;
-                }
-            }
-        }
-    }
-    if (boolean) {
-        printf("%s\n", s);
-        return 1;
-    } else {
+    if (strlen(s) != 9) {
         return 0;
     }
+    
+    if (s[0] != 'a' && s[0] != 'A') {
+        return 0;
+    }
+    
+    for (i = 1; i < 9; i++) {
+        if (!isdigit(s[i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 /*
@@ -76,4 +89,21 @@ int is_valid_id(const char s[]) {
 */
 int get_int(const char prompt[], int *p) {
 
+    int temp = 0;
+
+    char line[LINESIZE];
+    while (1) {
+        printf("%s", prompt);
+        if(!fgets(line, LINESIZE, stdin)) {
+            clearerr(stdin);
+            return 0;
+        }
+        if((sscanf(line, "%d", &temp) == 1) 
+            && (temp >= 0 && temp <= 100)) {
+            *p = temp;
+            return 1;
+        }
+    }    
+
 }
+
